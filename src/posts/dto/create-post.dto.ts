61df7a670db38d29adecf2e8c0db86bfa2fa0bba
Mitku <mitkudubale3864@gmail.com>
2023-09-02 22:56:47 +0300

    progress on r/ship

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,11 +1,19 @@
-import { IsNotEmpty, MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString, MinLength } from 'class-validator';
+import { Types } from 'mongoose';
 import { User } from 'src/users/entities/user.entity';
+
 export class CreatePostDto {
   @IsNotEmpty()
   @MinLength(5)
   title: string;
   @IsNotEmpty()
   body: string;
+  @ApiProperty({
+    type: Types.ObjectId,
+    description: 'ID of the author or User object',
+  })
+  @IsString()
   @IsNotEmpty()
-  author: User;
+  author: Types.ObjectId | User;
 }
